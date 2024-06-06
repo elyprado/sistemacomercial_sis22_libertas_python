9819ab88af311a37a2ce7dac96774616539635ea
Hiago Milaneze Rossi <135182031+Haiverhanm@users.noreply.github.com>
2024-06-06 18:32:07 -0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,13 +1,11 @@
 from flask import Flask, send_from_directory
-from usuario import usuario_bp
-from cliente import cliente_bp
+from cidade import cidade_bp
 import os
 
 app = Flask(__name__,   
 static_url_path='', 
             static_folder='static')
-app.register_blueprint(usuario_bp)
-app.register_blueprint(cliente_bp)
+app.register_blueprint(cidade_bp)
 
 @app.route("/")
 def home():
